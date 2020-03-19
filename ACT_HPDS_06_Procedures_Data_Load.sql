64f494eb14321779741c6f0410b0e3c61b613295
Jaspreet Khanna <Jaspreet.Khanna@childrens.harvard.edu>
2020-03-19 14:53:06 -0400

    ACT_HPDS_06_Procedures_Data_Load.sql
    Script to load procedures data in HPDS ACT format

---
+++ ACT_HPDS_06_Procedures_Data_Load.sql
@@ -0,0 +1,15 @@
+--Script for loading Procedures ACT fact data in HPDS.
+--Mapping table tm_cz.a_ACT_ICD10PCS_PX_2018AA_map has mapping for BCH Procedures to ACT Procedures concept_paths
+--It can be loaded from the csv file data_a_ACT_ICD10PCS_PX_2018AA_map.tar.gz
+--HPDS Diagnosis data is loaded in ACT format.
+
+
+
+insert into  tm_cz.HPDS_DATA_LATEST
+select  distinct fact1.patient_num, replace(cd.act_concept_path,'\ACT','') ,null,cd.act_name_char c_name 
+from i2b2demodata.observation_fact fact1, tm_cz.a_ACT_ICD10PCS_PX_2018AA_map cd
+where cd.bch_concept_cd = fact1.CONCEPT_CD;
+
+commit;
+
+
