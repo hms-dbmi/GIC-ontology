452dc341e30ae30d9d94a3130d538b3b5a806f77
Jaspreet Khanna <Jaspreet.Khanna@childrens.harvard.edu>
2020-03-19 14:03:10 -0400

    ACT_HPDS_02_Data_Load_Manual.sql
    Instructions and scripts to load the ontology mapping data.

---
+++ ACT_HPDS_02_Data_Load_Manual.sql
@@ -0,0 +1,14 @@
+--Initial data load script
+
+/*
+@data_insert_ncats_demographics.sql
+@data_insert_metadata_visits_oracle_V201.sql  
+
+--listed files have to unzipped and imported in corresponding tables
+data_A_ACT_ICD10PCS_PX_2018AA_MAP.tar.gz ( csv file for for mapping data of A_ACT_ICD10PCS_PX_2018AA_MAP)
+data_A_NCATS_ICD10_ICD9_DX_V1_MAP.tar.gz ( csv file for for mapping data of A_NCATS_ICD10_ICD9_DX_V1_MAP )
+
+*/
+
+@data_insert_ncats_demographics.sql
+@data_insert_metadata_visits_oracle_V201.sql  
