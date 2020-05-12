aedeec80441934c70699564eabd26e775c9474d9
Jaspreet Khanna <Jaspreet.Khanna@childrens.harvard.edu>
2020-05-12 15:14:26 -0400

    Created ACT_HPDS_06_Procedures_Data_Load.sql
    Created ACT_HPDS_06_Procedures_Data_Load.sql

---
+++ Previous/ACT_HPDS_06_Procedures_Data_Load.sql
@@ -0,0 +1,17 @@
+--Script for loading Procedures ACT fact data in HPDS.
+--Mapping table tm_cz.a_ACT_ICD10PCS_PX_2018AA_map has mapping for BCH Procedures to ACT Procedures concept_paths
+--It can be loaded from the csv file data_a_ACT_ICD10PCS_PX_2018AA_map.tar.gz
+
+
+set serveroutput on
+Begin
+insert into tm_cz.HPDS_DATA_LATEST( PATIENT_NUM,CONCEPT_PATH,NVAL_NUM,TVAL_CHAR,start_date)
+select  distinct fact1.patient_num, replace(cd.act_concept_path,'\ACT','') ,null,cd.act_name_char c_name ,start_date
+from i2b2demodata.observation_fact fact1, tm_cz.a_ACT_ICD10PCS_PX_2018AA_map cd
+where cd.bch_concept_cd = fact1.CONCEPT_CD;
+
+dbms_output.put_line( 'Inserted Rows '||sql%rowcount);
+commit;
+end;
+
+
